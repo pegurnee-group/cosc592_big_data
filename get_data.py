1f7cdbbdb30d5fefd38333527d2591742ebca9f5
Eddie Gurnee <pegurnee@gmail.com>
2016-03-14 05:07:34 -0400

    put some more information about the repo

--- get_data.py
+++ get_data.py
@@ -7,7 +7,8 @@ with open('credentials') as fp:
 g = Github(creds['username'], creds['password'])
 
 for repo in g.get_user().get_repos():
-    print (repo.name)
+    print (repo.name) #prints all of my repos
 
 for repo in g.get_repos():
-  print("{}: {}".format(repo.id, repo.full_name))
+  #prints public repos, id, language, size, name
+  print("{}: {} {} \n\t{}".format(repo.id, repo.language, repo.size, repo.full_name))
