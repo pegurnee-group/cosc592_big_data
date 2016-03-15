768dbebe8463c87bf0a5ea670024fe30ab7e78ff
Eddie Gurnee <pegurnee@gmail.com>
2016-03-15 06:14:53 -0400

    format some string, keys some data

--- filter_useful.py
+++ filter_useful.py
@@ -4,12 +4,12 @@ repos = {}
 with open('bigfile.json') as fp:
   for data in fp:
     jsdata = json.loads(data)
-    if not repos[jsdata['language']]:
+    if not jsdata['language'] in repos.keys():
       repos[jsdata['language']] = []
 
     repos[jsdata['language']].append(jsdata)
 
 for key in repos:
-  with open("{}.json") as fp:
+  with open("{}.json".format(key), 'w') as fp:
     for repo in repos[key]:
-      fp.write(repo)
+      fp.write("{}\n".format(repo)
