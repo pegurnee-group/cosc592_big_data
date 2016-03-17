9a2d368ebd1f834ad53f3dda27ff783020b87336
Eddie Gurnee <pegurnee@gmail.com>
2016-03-17 07:33:53 -0400

    clone the languages as requested

--- clone_repos.py
+++ clone_repos.py
@@ -1,10 +1,12 @@
 import json
 from git import Repo
 
-with open('filtered_out/PHP.json') as fp:
-  for jsline in fp:
-    repo = json.loads(jsline)
-    src = '{}.git'.format(repo['url'])
-    dest = 'code/{}/{}'.format(repo['language'], repo['id'])
-    print('cloned: '.format(src))
-    the_repo = Repo.clone_from(src, dest)
+languages = {'PHP', 'C', 'C#', 'C++', 'Java', 'JavaScript', 'Objective-C', 'PHP', 'Python', 'Ruby'}
+for language in languages:
+  with open('filtered_out/{}.json'.format(language)) as fp:
+    for jsline in fp:
+      repo = json.loads(jsline)
+      src = '{}.git'.format(repo['url'])
+      dest = 'code/{}/{}'.format(repo['language'], repo['id'])
+      print('cloned: {}'.format(src))
+      the_repo = Repo.clone_from(src, dest)
