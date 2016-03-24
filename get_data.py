ce3b485a06e45109dd6e81564da1862948557473
Eddie Gurnee <pegurnee@gmail.com>
2016-03-24 14:07:00 -0400

    add command line options

--- get_data.py
+++ get_data.py
@@ -1,16 +1,33 @@
-import json
+import json, sys
 
+from getopt import getopt
 from github import Github
 
-with open('credentials') as fp:
+optstring = 'f:s:n:'
+
+opts, args = getopt(sys.argv[1:], optstring)
+
+credentials_file  = 'credentials'
+start_value       = 0
+download_number   = 3000
+
+for o,v in opts:
+  if o == '-f':
+    credentials_file = v
+  elif o == '-s':
+    start_value = int(v)
+  elif o == '-n':
+    download_number = int(v)
+
+with open(credentials_file) as fp:
   creds = json.load(fp)
 
 g = Github(creds['username'], creds['password'])
 
 with open('in/bigfile.json', 'w') as outf:
-  counter = 0
+  counter = start_value
   increment = 25
-  LIMIT = counter + 500
+  LIMIT = counter + download_number
   while counter < LIMIT:
     for repo in g.get_repos()[counter : counter + increment]:
       #prints public repos: id, language, url
