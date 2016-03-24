db05f88f02be8987044225ec333e866578b412cb
Eddie Gurnee <pegurnee@gmail.com>
2016-03-24 14:36:14 -0400

    num repos

--- clone_repos.py
+++ clone_repos.py
@@ -4,15 +4,19 @@ from git import Repo
 languages = {'PHP', 'C', 'C#', 'C++', 'Java', 'JavaScript', 'Objective-C', 'PHP', 'Python', 'Ruby'}
 for language in languages:
   with open('filtered_out/{}.json'.format(language)) as fp:
+    num_repos = 0
     for jsline in fp:
       repo = json.loads(jsline)
       src = '{}.git'.format(repo['url'])
       dest = 'code/{}/{}'.format(repo['language'], repo['id'])
-      print('cloned: {}'.format(src))
 
       try:
         the_repo = Repo.clone_from(src, dest)
       except Exception as e:
-        pass
+        continue
+
+      num_repos += 1
+
+    print('cloned all {} {} repos'.format(num_repos, language))
 
 print('cloning finished')
