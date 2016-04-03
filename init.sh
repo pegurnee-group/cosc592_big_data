f89a73e2216b4493f0822b3a297ba41f4592c874
J2quared <jjohn226@emich.edu>
2016-04-03 18:07:07 -0400

    currently working on improving init.sh, need to test on machine without pip3

--- init.sh
+++ init.sh
@@ -1,2 +1,26 @@
-pip install PyGithub
-pip install GitPython
+which python3 #check if python3 installed
+if [ $? -eq 1 ]
+then
+    echo "Python3 is not installed, please install Python3 and run script again"
+    exit 1
+fi
+
+python3 -c "import setuptools" # check if pip3 is installed
+if [ $? -eq 0 ] #true
+then
+    echo "Installing PyGithub and GitPython"
+    pip3 install PyGithub
+    pip3 install GitPython
+else
+    echo "Pip is not installed!"
+    echo "Attempting to install Pip"
+    easy_install pip3
+    if [ $? -eq 1 ] #failed
+    then
+      echo "Install failed, try running as root or with 'sudo'"
+    else
+      echo "Pip was successfully installed, retrying PyGithub and GitPython installation"
+      pip3 install PyGithub
+      pip3 install GitPython
+    fi
+fi
