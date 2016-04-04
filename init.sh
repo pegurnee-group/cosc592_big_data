8c7e02bc386460e48852946061bddf7398577a4e
J2quared <jjohn226@emich.edu>
2016-04-04 12:08:30 -0400

    working on sudo versus non sudo execution, because eddie is super lazy about installing python3

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-which python3 > /dev/null 2>&1 #check if python3 installed
+which python3 > /dev/null 2>&1  #check if python3 installed
 if [ $? -eq 1 ]
 then
     echo "Python3 is not installed, attempting installation, if failed try running as sudoer"
     echo "Detecting platform"
     python -mplatform | grep -qi Ubuntu && sudo apt-get install python3
-    python -mplatform | grep -qi CentOS && sudo yum install python3
+    python -mplatform | grep -qi CentOS && yum install yum-utils && yum-builddep python && curl -O https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz   && tar xf Python-3.5.0.tgz && cd Python-3.5.0 && ./configure && make && make install && ln -s /usr/local/bin/python3 /usr/bin/python3 
     python -mplatform | grep -qi Darwin && brew install python3
     if [ $? -eq 0 ]
     then
@@ -21,7 +21,7 @@ then
 else
     echo "Pip is not installed!"
     echo "Attempting to install Pip"
-    easy_install pip3
+    easy_install3 pip
     if [ $? -eq 1 ] #failed
     then
       echo "Install failed, try running as root or with 'sudo'"
