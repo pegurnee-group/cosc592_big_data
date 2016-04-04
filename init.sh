fa3e9bea053a03ae5b1df179b5208f686a7114c1
J2quared <jjohn226@emich.edu>
2016-04-04 01:17:30 -0400

    small bug which requires init to be ran twice, working on fix, however OS detection works

--- init.sh
+++ init.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
 which python3 > /dev/null 2>&1 #check if python3 installed
 if [ $? -eq 1 ]
 then
-    echo "Python3 is not installed, please install Python3 and run script again"
-    exit 1
+    echo "Python3 is not installed, attempting installation, if failed try running as sudoer"
+    echo "Detecting platform"
+    python -mplatform | grep -qi Ubuntu && sudo apt-get install python3
+    python -mplatform | grep -qi CentOS && sudo yum install python3
+    python -mplatform | grep -qi Darwin && brew install python3
+    if [ $? -eq 0 ]
+    then
+      echo "Python3 installed"
+    fi
 fi
-
 python3 -c "import setuptools" # check if pip3 is installed
 if [ $? -eq 0 ] #true
 then
@@ -22,5 +29,6 @@ else
       echo "Pip was successfully installed, retrying PyGithub and GitPython installation"
       pip3 install PyGithub
       pip3 install GitPython
+      echo "Updating and upgrading system"
     fi
 fi
