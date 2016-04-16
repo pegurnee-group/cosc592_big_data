863d1c0353e71043da028138e40b51f0fbc153a0
Eddie Gurnee <egurnee@emich.edu>
2016-04-15 17:51:44 -0700

    update sao

--- project/src/src/StyleAnalysisObject.java
+++ project/src/src/StyleAnalysisObject.java
@@ -9,10 +9,12 @@ import org.apache.hadoop.io.Writable;
 public class StyleAnalysisObject implements Writable {
 	private int numberCloseBraces;
 	private int numberOpenBracesOnOwnLine;
+	private int numberOfInlineComments;
 
 	public StyleAnalysisObject() {
 		this.numberCloseBraces = 0;
 		this.numberOpenBracesOnOwnLine = 0;
+		this.numberOfInlineComments = 0;
 	}
 
 	public StyleAnalysisObject(int numberCloseBraces,
@@ -20,17 +22,26 @@ public class StyleAnalysisObject implements Writable {
 		this.numberCloseBraces = numberCloseBraces;
 		this.numberOpenBracesOnOwnLine = numberOpenBracesOnOwnLine;
 	}
+	
+	public StyleAnalysisObject(int numberCloseBraces,
+			int numberOpenBracesOnOwnLine, int numOfInlineComments) {
+			this.numberCloseBraces = numberCloseBraces;
+			this.numberOpenBracesOnOwnLine = numberOpenBracesOnOwnLine;
+			this.numberOfInlineComments = numOfInlineComments;
+	}
 
 	@Override
 	public void readFields(DataInput in) throws IOException {
 		numberCloseBraces = in.readInt();
 		numberOpenBracesOnOwnLine = in.readInt();
+		numberOfInlineComments = in.readInt();
 	}
 
 	@Override
 	public void write(DataOutput out) throws IOException {
 		out.writeInt(numberCloseBraces);
 		out.writeInt(numberOpenBracesOnOwnLine);
+		out.writeInt(numberOfInlineComments);
 	}
 
 	public int getNumberCloseBraces() {
@@ -39,6 +50,10 @@ public class StyleAnalysisObject implements Writable {
 
 	public int getNumberOpenBracesOnOwnLine() {
 		return numberOpenBracesOnOwnLine;
+	}
+	
+	public int getNumberOfInlineComments() {
+		return numberOfInlineComments;
 	}
 
 }
