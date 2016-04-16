0a5c49a7182985957d65937b622cb06be8d93778
Eddie Gurnee <egurnee@emich.edu>
2016-04-15 18:01:52 -0700

    input formattter

---
+++ project/src/src/StyleAnalysisInputFormat.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+public class StyleAnalysisInputFormat extends FileInputFormat<Text, Text> {
+
+	@Override
+	public StyleAnalysisRecordReader createRecordReader(InputSplit arg0,
+			TaskAttemptContext arg1) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+private class StyleAnalysisRecordReader extends RecordReader<Text, Text>{
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Text getCurrentKey() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Text getCurrentValue() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getProgress() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initialize(InputSplit arg0, TaskAttemptContext arg1)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean nextKeyValue() throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
+}
