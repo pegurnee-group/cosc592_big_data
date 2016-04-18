a744cac7f2a3c67d1f8a4de88f8f3c20ba866bfb
Eddie Gurnee <egurnee@emich.edu>
2016-04-17 22:13:05 -0700

    putting is better

--- hdfs_put.sh
+++ hdfs_put.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+SOURCE='source_code'
+
+if ! hdfs dfs -test -d $SOURCE; then
+  hdfs dfs -mkdir $SOURCE
+
 for file in $(find ./code -iname '*.java' -or -iname '*.c' -or -iname '*.js'); do
-  hdfs dfs -put $file source_code
+  REPO_NUM=$( echo $file | awk -F'[/]' '{print $4}' )
+  if ! hdfs dfs -test -d $SOURCE/$REPO_NUM; then
+    hdfs dfs -mkdir $SOURCE/$REPO_NUM
+  fi
+  hdfs dfs -put $file $SOURCE/$REPO_NUM/
 done
