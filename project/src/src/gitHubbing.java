8def2e871d73a9aead798eae9df9993fb38c91c4
Eddie Gurnee <egurnee@emich.edu>
2016-04-18 05:35:08 -0700

    setInputDirRecursive yes please

--- project/src/src/gitHubbing.java
+++ project/src/src/gitHubbing.java
@@ -1,6 +1,10 @@
 package src;
 
+import java.util.ArrayList;
+
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -18,14 +22,18 @@ public class gitHubbing {
 		job.setJarByClass(gitHubbing.class);
 		job.setJobName("gitHubbing");
 
+		final ArrayList<FileStatus> files = new ArrayList<FileStatus>();
+
+		FileInputFormat.setInputDirRecursive(job, true);
+
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		job.setMapperClass(StyleAnalysisMapper.class);
 		job.setReducerClass(StyleAnalysisReducer.class);
 
-		job.setOutputKeyClass(StyleAnalysisKey.class);
-		job.setOutputValueClass(StyleAnalysisOutput.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(StyleAnalysisObject.class);
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
