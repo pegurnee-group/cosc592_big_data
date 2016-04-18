0480becd4f8305323e4d7a0792555adf23be3842
Eddie Gurnee <egurnee@emich.edu>
2016-04-18 10:38:44 -0700

    up date hdfs to output

--- hdfs_put.sh
+++ hdfs_put.sh
@@ -6,10 +6,13 @@ if ! hdfs dfs -test -d $SOURCE; then
   hdfs dfs -mkdir $SOURCE
 fi
 
+echo "source folder made"
+
 for file in $(find ./code -iname '*.java' -or -iname '*.c' -or -iname '*.js'); do
   REPO_NUM=$( echo $file | awk -F'[/]' '{print $4}' )
   if ! hdfs dfs -test -d $SOURCE/$REPO_NUM; then
     hdfs dfs -mkdir $SOURCE/$REPO_NUM
   fi
   hdfs dfs -put $file $SOURCE/$REPO_NUM/
+  echo "put file $file"
 done
